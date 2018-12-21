a7a2ec0516a28b403bbc4ce89c9d6de5271d1cdb
Daniel Irvine <daniel@danielirvine.com>
2018-12-21 18:38:58 +0000

    Use function lookup

--- src/parser.js
+++ src/parser.js
@@ -22,21 +22,18 @@ function rotate({ turtle, drawCommands }, addAngle) {
   };
 }
 
-const forward = (state, args) => moveDistance(state, parseInt(args[0]));
-const backward = (state, args) => moveDistance(state, -parseInt(args[0]));
-const left = (state, args) => rotate(state, parseInt(args[0]));
-const right = (state, args) => rotate(state, -parseInt(args[0]));
+const builtInFunctions = {
+  forward: (state, args) => moveDistance(state, parseInt(args[0])),
+  backward: (state, args) => moveDistance(state, -parseInt(args[0])),
+  left: (state, args) => rotate(state, -parseInt(args[0])),
+  right: (state, args) => rotate(state, parseInt(args[0]))
+};
 
 export function parseLine(line, state) {
   const [ functionName, ...rest ] = line.split(' ');
-  if (functionName == 'forward') {
-    return forward(state, rest);
-  } else if (functionName == 'backward') {
-    return backward(state, rest);
-  } else if (functionName == 'right') {
-    return left(state, rest);
-  } else if (functionName == 'left') {
-    return right(state, rest);
+  const foundFunction = builtInFunctions[functionName];
+  if (foundFunction) {
+    return foundFunction(state, rest);
   } else {
     return {
       ...state,
