6952f1846a81e627b4baa28e3db5bd2dc0112b42
Daniel Irvine <daniel@danielirvine.com>
2018-12-24 11:31:06 -0500

    Do not draw lines if pen is up

--- src/language/moveDistance.js
+++ src/language/moveDistance.js
@@ -1,22 +1,24 @@
 const radians = angle => Math.PI * angle / 180;
 
 export function moveDistance(state, distanceValue) {
-  const { drawCommands, turtle } = state;
-  let angle = turtle.angle;
+  let { drawCommands, turtle } = state;
+  const angle = turtle.angle;
   const radius = distanceValue.get(state);
-  let newX = turtle.x + Math.cos(radians(angle)) * radius;
+  const newX = turtle.x + Math.cos(radians(angle)) * radius;
   const newY = turtle.y + Math.sin(radians(angle)) * radius;
 
+  if (state.pen.down) {
+    drawCommands = [ ...drawCommands, {
+      drawCommand: 'drawLine',
+      x1: turtle.x,
+      y1: turtle.y,
+      x2: newX,
+      y2: newY
+    }];
+  }
+
   return {
-    drawCommands: [
-      ...drawCommands,
-      { drawCommand: 'drawLine',
-        x1: turtle.x,
-        y1: turtle.y,
-        x2: newX,
-        y2: newY
-      }
-    ],
+    drawCommands,
     turtle: {
       ...turtle,
       x: newX,
--- test/language/moveDistance.test.js
+++ test/language/moveDistance.test.js
@@ -2,6 +2,8 @@ import { moveDistance } from '../../src/language/moveDistance';
 
 const value = v => ({ get: () => v });
 
+const initialState = { drawCommands: [], turtle: {x: 0, y: 0, angle: 0 }, pen: { down: true } };
+
 describe('moveDistance', () => {
   let result;
 
@@ -11,36 +13,36 @@ describe('moveDistance', () => {
 
   describe('when angle is 0', () => {
     it('increases turtle x', () => {
-      doMove({ drawCommands: [], turtle: { x: 0, y: 0, angle: 0 } }, 100);
+      doMove(initialState, 100);
       expect(result.turtle.x).toEqual(100);
     });
 
     it('adds a new draw command when moving forward', () => {
-      doMove({ drawCommands: [], turtle: { x: 0, y: 0, angle: 0 } }, 100);
+      doMove(initialState, 100);
       expect(result.drawCommands).toEqual([
         { drawCommand: 'drawLine', x1: 0, y1: 0, x2: 100, y2: 0 }
       ]);
     });
 
     it('maintains existing draw commands', () => {
-      doMove({ drawCommands: [1, 2, 3], turtle: { x: 0, y: 0, angle: 0 } }, 100);
+      doMove({ ...initialState, drawCommands: [1, 2, 3] }, 100);
       expect(result.drawCommands.slice(0, 3)).toEqual([1, 2, 3]);
     });
 
     it('maintains existing turtle properties', () => {
-      doMove({ drawCommands: [], turtle: { x: 0, y: 0, angle: 0 } }, 100);
+      doMove(initialState, 100);
       expect(result.turtle.angle).toEqual(0);
     });
 
     it('descreases x when moving with a negative direction', () => {
-      doMove({ a: 123, drawCommands: [], turtle: { x: 0, y: 0, angle: 0 } }, -100);
+      doMove(initialState, -100);
       expect(result.turtle.x).toEqual(-100);
     });
   });
 
   describe('when angle is 90', () => {
     it('increases turtle y', () => {
-      doMove({ drawCommands: [], turtle: { x: 0, y: 0, angle: 90 } }, 100);
+      doMove({ ...initialState, turtle: { x: 0, y: 0, angle: 90 } }, 100);
       expect(result.turtle.y).toEqual(100);
     });
   });
@@ -48,13 +50,20 @@ describe('moveDistance', () => {
   const radians = angle => Math.PI * angle / 180;
   describe('when angle is 30', () => {
     it('uses cos to calculate x', () => {
-      doMove({ drawCommands: [], turtle: { x: 0, y: 0, angle: 30 } }, 100);
+      doMove({ ...initialState, turtle: { x: 0, y: 0, angle: 30 } }, 100);
       expect(result.turtle.x).toEqual(Math.cos(radians(30)) * 100);
     });
 
     it('uses sin to calculate y', () => {
-      doMove({ drawCommands: [], turtle: { x: 0, y: 0, angle: 30 } }, 100);
+      doMove({ ...initialState, turtle: { x: 0, y: 0, angle: 30 } }, 100);
       expect(result.turtle.y).toEqual(Math.sin(radians(30)) * 100);
+    });
+  });
+
+  describe('penup', () => {
+    it('does not draw line if pen is up', () => {
+      doMove({ ...initialState, pen: { down: false } }, 10);
+      expect(result.drawCommands).toEqual([]);
     });
   });
 });
