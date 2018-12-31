6273d8c0eb9379da04792b0b52d786267ad96514
Daniel Irvine <daniel@danielirvine.com>
2018-12-31 13:42:39 -0500

    Add StatementHistory component

---
+++ src/StatementHistory.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const groupByLineNumber = tokens => {
+  return tokens.reduce((lines, token) => {
+    if (lines[token.lineNumber]) {
+      return { ...lines, [token.lineNumber]: [ ...lines[token.lineNumber], token ] };
+    } else {
+      return { ...lines, [token.lineNumber]: [ token ] };
+    }
+  }, {});
+};
+
+export const LineWithNumber = ({ number, tokens }) => {
+  const fullTextLine = tokens.map(instruction => instruction.text).join('');
+  return (
+    <tr key={number.toString()}>
+      <td className="lineNumber">{number}</td>
+      <td className="text">{fullTextLine}</td>
+    </tr>
+  );
+};
+
+export const StatementHistory = connect(({ script: { parsedTokens } }) => ({ parsedTokens }), {})(({ parsedTokens }) => {
+  const lines = groupByLineNumber(parsedTokens);
+
+  return (
+    <tbody key="acceptedStatements">
+      {Object.keys(lines).map(lineNumber => <LineWithNumber key={lineNumber} number={lineNumber} tokens={lines[lineNumber]} />)}
+    </tbody>
+  );
+});
