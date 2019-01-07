0f5562308f3143eea117b14528842495a771557e
Daniel Irvine <daniel@danielirvine.com>
2019-01-07 09:40:46 +0000

    Animate drawing of lines using SVG animate

--- src/ScriptOutput.js
+++ src/ScriptOutput.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import { useMappedState } from 'redux-react-hook';
-const { useCallback } = React;
+const { useCallback, useRef, useState } = React;
 
 export const ScriptOutput = () => {
+  const duration = 0.5;
   const mapState = useCallback(({ script: { present: { drawCommands } } }) => ({ drawCommands }), []);
   const { drawCommands } = useMappedState(mapState);
 
+  const svgRef = useRef();
+
+  const [ animationTimes, setAnimationTimes ] = useState({});
+
+  let currentTime;
+  if (svgRef.current) {
+    currentTime = svgRef.current.getCurrentTime();
+  } else {
+    currentTime = 0;
+  }
+
+  const newAnimationTimes = drawCommands.reduce((times, command) => {
+    if (times.newAnimationTimes[command.id] === undefined) {
+      return {
+        newAnimationTimes: {
+          ...times.newAnimationTimes,
+          [command.id]: times.nextBeginTime
+        },
+        nextBeginTime: times.nextBeginTime + duration
+      };
+    }
+    return times;
+  }, { newAnimationTimes: animationTimes, nextBeginTime: currentTime }).newAnimationTimes;
+
+  if (newAnimationTimes != animationTimes) {
+    setAnimationTimes(newAnimationTimes);
+  }
+
   return (
     <div id="viewport">
-      <svg viewBox="-300 -300 600 600" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
-        {drawCommands.map(({ id, x1, y1, x2, y2 }) => <line key={id} x1={x1} y1={y1} x2={x2} y2={y2} strokeWidth="2" stroke="black" />)}
+      <svg viewBox="-300 -300 600 600" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg" ref={svgRef}>
+        {drawCommands.map(({ id, x1, y1, x2, y2 }) => {
+          return <line key={id} x1={x1} y1={y1} x2={x1} y2={y1} strokeWidth="2" stroke="black">
+            <animate attributeName="x2" begin={newAnimationTimes[id]} dur={duration} to={x2} fill="freeze" />
+            <animate attributeName="y2" begin={newAnimationTimes[id]} dur={duration} to={y2} fill="freeze" />
+        </line>})}
       </svg>
     </div>
   );
--- test/ScriptOutput.test.js
+++ test/ScriptOutput.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { mount } from 'enzyme';
 import { StoreContext } from 'redux-react-hook';
 import { expectRedux, storeSpy } from 'expect-redux';
@@ -10,13 +11,7 @@ describe('ScriptOutput', () => {
   let wrapper;
 
   beforeEach(() => {
-    store = configureStore([storeSpy], { script: { present: {
-      drawCommands: [
-        { id: 123, x1: 234, y1: 345, x2: 456, y2: 567 },
-        { id: 234, x1: 234, y1: 345, x2: 456, y2: 567 },
-        { id: 567, x1: 234, y1: 345, x2: 456, y2: 567 }
-      ]
-    }}});
+    store = configureStore([storeSpy]);
   });
 
   function mountWithStore(component) {
@@ -25,6 +20,10 @@ describe('ScriptOutput', () => {
 
   function svg() {
     return wrapper.find('svg');
+  }
+
+  function drawLine() {
+    store.dispatch({ type: 'SUBMIT_EDIT_LINE', text: 'forward 10\n' });
   }
 
   it('renders an svg inside div#viewport', () => {
@@ -40,26 +39,139 @@ describe('ScriptOutput', () => {
   });
 
   it('draws a line for a draw command in state', () => {
+    drawLine();
     wrapper = mountWithStore(<ScriptOutput />);
     expect(svg().childAt(0).type()).toEqual('line');
-    expect(svg().childAt(0).prop('x1')).toEqual(234);
-    expect(svg().childAt(0).prop('y1')).toEqual(345);
-    expect(svg().childAt(0).prop('x2')).toEqual(456);
-    expect(svg().childAt(0).prop('y2')).toEqual(567);
+    expect(svg().childAt(0).prop('x1')).toEqual(0);
+    expect(svg().childAt(0).prop('y1')).toEqual(0);
+  });
+
+  it('initially sets end of line to beginning of line, ready for animation', () => {
+    drawLine();
+    expect(svg().childAt(0).prop('x2')).toEqual(0);
+    expect(svg().childAt(0).prop('y2')).toEqual(0);
   });
 
   it('sets a stroke width of 2 on each line', () => {
+    drawLine();
     wrapper = mountWithStore(<ScriptOutput />);
     expect(svg().childAt(0).prop('strokeWidth')).toEqual('2');
   });
 
   it('sets a stroke color of black on each line', () => {
+    drawLine();
     wrapper = mountWithStore(<ScriptOutput />);
     expect(svg().childAt(0).prop('stroke')).toEqual('black');
   });
 
   it('draws every command', () => {
+    drawLine();
+    drawLine();
+    drawLine();
     wrapper = mountWithStore(<ScriptOutput />);
     expect(svg().children().length).toEqual(3);
+  });
+
+  describe('animating', () => {
+    describe('x2 coordinate', () => {
+      it('renders animate element', () => {
+        drawLine();
+        wrapper = mountWithStore(<ScriptOutput />);
+        const firstLine = svg().childAt(0);
+        expect(firstLine.childAt(0).type()).toEqual('animate');
+        expect(firstLine.childAt(0).prop('attributeName')).toEqual('x2');
+        expect(firstLine.childAt(0).prop('to')).toEqual(10);
+      });
+
+      it('sets the begin time to 0 when component first mounts', () => {
+        drawLine();
+        wrapper = mountWithStore(<ScriptOutput />);
+        const firstLine = svg().childAt(0);
+        expect(firstLine.childAt(0).prop('begin')).toEqual(0);
+      });
+
+      it('has a duration of 0.5s', () => {
+        drawLine();
+        wrapper = mountWithStore(<ScriptOutput />);
+        const firstLine = svg().childAt(0);
+        expect(firstLine.childAt(0).prop('dur')).toEqual(0.5);
+      });
+
+      it('sets the fill to freeze', () => {
+        drawLine();
+        wrapper = mountWithStore(<ScriptOutput />);
+        const firstLine = svg().childAt(0);
+        expect(firstLine.childAt(0).prop('fill')).toEqual('freeze');
+      });
+    });
+
+    describe('y2 coordinate', () => {
+      it('renders animate element', () => {
+        drawLine();
+        wrapper = mountWithStore(<ScriptOutput />);
+        const firstLine = svg().childAt(0);
+        expect(firstLine.childAt(1).type()).toEqual('animate');
+        expect(firstLine.childAt(1).prop('attributeName')).toEqual('y2');
+        expect(firstLine.childAt(1).prop('to')).toEqual(0);
+      });
+
+      it('sets the begin time to 0 when component first mounts', () => {
+        drawLine();
+        wrapper = mountWithStore(<ScriptOutput />);
+        const firstLine = svg().childAt(0);
+        expect(firstLine.childAt(1).prop('begin')).toEqual(0);
+      });
+
+      it('has a duration of 0.5s', () => {
+        drawLine();
+        wrapper = mountWithStore(<ScriptOutput />);
+        const firstLine = svg().childAt(0);
+        expect(firstLine.childAt(1).prop('dur')).toEqual(0.5);
+      });
+
+      it('sets the fill to freeze', () => {
+        drawLine();
+        wrapper = mountWithStore(<ScriptOutput />);
+        const firstLine = svg().childAt(0);
+        expect(firstLine.childAt(1).prop('fill')).toEqual('freeze');
+      });
+    });
+
+    it('creates x2 and y2 animations for every line drawn', () => {
+      drawLine();
+      drawLine();
+      drawLine();
+      wrapper = mountWithStore(<ScriptOutput />);
+      expect(wrapper.find('animate').length).toEqual(6);
+    });
+
+    describe('later draw commands', () => {
+      let timeSpy;
+      let root;
+
+      beforeEach(async () => {
+        timeSpy = jest.fn();
+        timeSpy.mockReturnValue(5);
+        SVGSVGElement.prototype.getCurrentTime = timeSpy;
+        root = document.createElement('div');
+        wrapper = mountWithStore(<ScriptOutput />);
+        drawLine();
+        drawLine();
+        await new Promise(setTimeout);
+        wrapper = wrapper.update();
+      });
+
+      it('uses getCurrentTime function to find current document animation time on second render', () => {
+        expect(timeSpy).toHaveBeenCalled();
+      });
+
+      it('sets the first new draw command begin to the current time', () => {
+        expect(wrapper.find('line').at(0).childAt(0).prop('begin')).toEqual(5);
+      });
+
+      it('sets the next draw command to that time plus the duration', () => {
+        expect(wrapper.find('line').at(1).childAt(0).prop('begin')).toEqual(5.5);
+      });
+    });
   });
 });
