61406d4105ec4014b1a5c01c18b5f0028173ad3f
Daniel Irvine <daniel@danielirvine.com>
2019-01-08 14:39:29 +0000

    Refactor to use requestAnimationFrame

--- src/Display.js
+++ src/Display.js
@@ -1,46 +1,68 @@
 import React from 'react';
 import { useMappedState } from 'redux-react-hook';
-const { useCallback, useRef, useState } = React;
+const { useCallback, useState, useEffect } = React;
+
+export const DrawingLine = ({ x1, y1, x2, y2, delay, duration }) => {
+  const [ endTime, setEndTime ] = useState();
+  const [ currentX2, setCurrentX2 ] = useState(x1);
+  const [ currentY2, setCurrentY2 ] = useState(y1);
+
+  useEffect(() => {
+    let start;
+    let timeoutId;
+    let animationFrameId;
+
+    const handleFrame = (time) => {
+      if (start === undefined) start = time;
+      if (time < start + duration) {
+        const elapsed = time - start;
+        setCurrentX2(x1 + ((x2 - x1) * (elapsed / duration)));
+        setCurrentY2(y1 + ((y2 - y1) * (elapsed / duration)));
+        requestAnimationFrame(handleFrame);
+      } else {
+        setCurrentX2(x2);
+        setCurrentY2(y2);
+      }
+    };
+    setTimeout(() => {
+      animationFrameId = requestAnimationFrame(handleFrame)
+    }, delay);
+
+    return () => {
+      clearTimeout(timeoutId);
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, []);
+
+  return <line x1={x1} y1={y1} x2={currentX2} y2={currentY2} strokeWidth="2" stroke="black" />;
+};
 
 export const Drawing = ({ drawCommands }) => {
-  const duration = 0.5;
+  const duration = 500;
 
-  const svgRef = useRef();
+  const [ delays, setDelays ] = useState({});
 
-  const [ animationTimes, setAnimationTimes ] = useState({});
-
-  let currentTime;
-  if (svgRef.current) {
-    currentTime = svgRef.current.getCurrentTime();
-  } else {
-    currentTime = 0;
-  }
-
-  const newAnimationTimes = drawCommands.reduce((times, command) => {
-    if (times.newAnimationTimes[command.id] === undefined) {
-      return {
-        newAnimationTimes: {
-          ...times.newAnimationTimes,
-          [command.id]: times.nextBeginTime
-        },
-        nextBeginTime: times.nextBeginTime + duration
-      };
+  let delay = 0;
+  const newDelays = drawCommands.reduce((delays, command) => {
+    if (delays[command.id] !== undefined) {
+      return delays;
+    } else {
+      const thisDelay = delay;
+      delay += duration;
+      return { ...delays, [command.id]: thisDelay };
     }
-    return times;
-  }, { newAnimationTimes: animationTimes, nextBeginTime: currentTime }).newAnimationTimes;
+  }, delays);
 
-  if (newAnimationTimes != animationTimes) {
-    setAnimationTimes(newAnimationTimes);
+  if (delay !== 0) {
+    setDelays(newDelays);
   }
 
   return (
     <div id="viewport">
-      <svg viewBox="-300 -300 600 600" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg" ref={svgRef}>
+      <svg viewBox="-300 -300 600 600" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
         {drawCommands.map(({ id, x1, y1, x2, y2 }) => {
-          return <line key={id} x1={x1} y1={y1} x2={x1} y2={y1} strokeWidth="2" stroke="black">
-            <animate attributeName="x2" begin={newAnimationTimes[id]} dur={duration} to={x2} fill="freeze" />
-            <animate attributeName="y2" begin={newAnimationTimes[id]} dur={duration} to={y2} fill="freeze" />
-        </line>})}
+          return <DrawingLine key={id} x1={x1} y1={y1} x2={x2} y2={y2} duration={duration} delay={delays[id]} />
+        })}
       </svg>
     </div>
   );
--- test/Display.test.js
+++ test/Display.test.js
@@ -1,25 +1,120 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { StoreContext } from 'redux-react-hook';
 import { expectRedux, storeSpy } from 'expect-redux';
 import { configureStore } from '../src/store';
-import { Drawing, ReduxConnectedDisplay } from '../src/Display';
+import { DrawingLine, Drawing, ReduxConnectedDisplay } from '../src/Display';
 
 let lineA = { id: 123, x1: 10, y1: 10, x2: 20, y2: 20 };
 let lineB = { id: 234, x1: 10, y1: 10, x2: 20, y2: 20 };
 let lineC = { id: 235, x1: 10, y1: 10, x2: 20, y2: 20 };
 
+describe('DrawingLine', () => {
+  let wrapper;
+  let requestAnimationFrameSpy;
+
+  beforeEach(() => {
+    requestAnimationFrameSpy = jest.fn();
+    window.requestAnimationFrame = requestAnimationFrameSpy;
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  function line() {
+    return wrapper.find('line');
+  }
+
+  function mountDrawingLine(props) {
+    wrapper = mount(<svg><DrawingLine {...lineA} {...props} /></svg>);
+  }
+
+  function triggerRequestAnimationFrame(time) {
+    const frameFn = requestAnimationFrameSpy.mock.calls[0][0];
+    frameFn(time);
+  }
+
+  it('draws a line for a draw command in state', () => {
+    mountDrawingLine({ delay: 0 });
+    expect(line().exists()).toBeTruthy();
+    expect(line().type()).toEqual('line');
+    expect(line().prop('x1')).toEqual(10);
+    expect(line().prop('y1')).toEqual(10);
+  });
+
+  it('initially sets end of line to beginning of line, ready for animation', () => {
+    mountDrawingLine({ delay: 0 });
+    expect(line().prop('x2')).toEqual(10);
+    expect(line().prop('y2')).toEqual(10);
+  });
+
+  it('sets a stroke width of 2 on each line', () => {
+    mountDrawingLine({ delay: 0 });
+    expect(line().prop('strokeWidth')).toEqual('2');
+  });
+
+  it('sets a stroke color of black on each line', () => {
+    mountDrawingLine({ delay: 0 });
+    expect(line().prop('stroke')).toEqual('black');
+  });
+
+  describe('setTimeout call', () => {
+    let setTimeoutSpy;
+    let setTimeoutOriginal;
+
+    beforeEach(() => {
+      setTimeoutOriginal = window.setTimeout;
+      setTimeoutSpy = jest.fn(window.setTimeout);
+      window.setTimeout = setTimeoutSpy;
+    });
+
+    afterEach(() => {
+      window.setTimeout = setTimeoutOriginal;
+    });
+
+    it('triggers a setTimeout to run after the delay', async () => {
+      mountDrawingLine({ delay: 1 });
+      await new Promise(setTimeoutOriginal);
+      expect(setTimeoutSpy).toHaveBeenLastCalledWith(expect.anything(), 1);
+    });
+  });
+
+  it('invokes requestAnimationFrame when the timeout fires', async () => {
+    mountDrawingLine({ delay: 0 });
+    await new Promise(setTimeout);
+    await new Promise(setTimeout);
+    expect(requestAnimationFrameSpy).toHaveBeenCalled();
+  });
+
+  it('renders proportionate x2 and y2 values after invoking the requestAnimationFrame handler with a duration', async () => {
+    mountDrawingLine({ delay: 0, duration: 500 });
+    await new Promise(setTimeout);
+    await new Promise(setTimeout);
+    triggerRequestAnimationFrame(0);
+    triggerRequestAnimationFrame(250);
+    wrapper = wrapper.update();
+    expect(line().prop('x2')).toEqual(15);
+    expect(line().prop('y2')).toEqual(15);
+  });
+
+  it('invokes requestAnimationFrame again when time is not up', async () => {
+    mountDrawingLine({ delay: 0, duration: 500 });
+    await new Promise(setTimeout);
+    await new Promise(setTimeout);
+    triggerRequestAnimationFrame(0);
+    expect(requestAnimationFrameSpy.mock.calls.length).toEqual(2);
+  });
+});
+
+// TODO: would be better if these tests used shallow
 describe('Drawing', () => {
   let store;
   let wrapper;
 
   function svg() {
     return wrapper.find('svg');
-  }
-
-  function drawLine() {
-    store.dispatch({ type: 'SUBMIT_EDIT_LINE', text: 'forward 10\n' });
   }
 
   it('renders an svg inside div#viewport', () => {
@@ -34,127 +129,25 @@ describe('Drawing', () => {
     expect(svg().prop('preserveAspectRatio')).toEqual('xMidYMid slice');
   });
 
-  it('draws a line for a draw command in state', () => {
-    wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-    expect(svg().childAt(0).type()).toEqual('line');
-    expect(svg().childAt(0).prop('x1')).toEqual(10);
-    expect(svg().childAt(0).prop('y1')).toEqual(10);
+  it('renders a DrawingLine with a 500ms duration', () => {
+    wrapper = mount(<Drawing drawCommands={[ lineA ]} />);
+    expect(wrapper.find('DrawingLine').exists()).toBeTruthy();
+    expect(wrapper.find('DrawingLine').containsMatchingElement(
+      <DrawingLine key={123} x1={10} y1={10} x2={20} y2={20} duration={500} delay={0} />
+    )).toBeTruthy();
   });
 
-  it('initially sets end of line to beginning of line, ready for animation', () => {
-    wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-    expect(svg().childAt(0).prop('x2')).toEqual(10);
-    expect(svg().childAt(0).prop('y2')).toEqual(10);
+  it('sets an incrementing on each draw command', () => {
+    wrapper = mount(<Drawing drawCommands={ [ lineA, lineB ] }/>);
+    const delays = wrapper.find('DrawingLine').map(line => line.prop('delay'));
+    expect(delays).toEqual([0, 500]);
   });
 
-  it('sets a stroke width of 2 on each line', () => {
-    wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-    expect(svg().childAt(0).prop('strokeWidth')).toEqual('2');
-  });
-
-  it('sets a stroke color of black on each line', () => {
-    wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-    expect(svg().childAt(0).prop('stroke')).toEqual('black');
-  });
-
-  it('draws every command', () => {
-    wrapper = mount(<Drawing drawCommands={ [ lineA, lineB, lineC ] }/>);
-    expect(svg().children().length).toEqual(3);
-  });
-
-  describe('animating', () => {
-    describe('x2 coordinate', () => {
-      it('renders animate element', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-        const firstLine = svg().childAt(0);
-        expect(firstLine.childAt(0).type()).toEqual('animate');
-        expect(firstLine.childAt(0).prop('attributeName')).toEqual('x2');
-        expect(firstLine.childAt(0).prop('to')).toEqual(20);
-      });
-
-      it('sets the begin time to 0 when component first mounts', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-        const firstLine = svg().childAt(0);
-        expect(firstLine.childAt(0).prop('begin')).toEqual(0);
-      });
-
-      it('has a duration of 0.5s', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-        const firstLine = svg().childAt(0);
-        expect(firstLine.childAt(0).prop('dur')).toEqual(0.5);
-      });
-
-      it('sets the fill to freeze', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-        const firstLine = svg().childAt(0);
-        expect(firstLine.childAt(0).prop('fill')).toEqual('freeze');
-      });
-    });
-
-    describe('y2 coordinate', () => {
-      it('renders animate element', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-        const firstLine = svg().childAt(0);
-        expect(firstLine.childAt(1).type()).toEqual('animate');
-        expect(firstLine.childAt(1).prop('attributeName')).toEqual('y2');
-        expect(firstLine.childAt(1).prop('to')).toEqual(20);
-      });
-
-      it('sets the begin time to 0 when component first mounts', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-        const firstLine = svg().childAt(0);
-        expect(firstLine.childAt(1).prop('begin')).toEqual(0);
-      });
-
-      it('has a duration of 0.5s', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-        const firstLine = svg().childAt(0);
-        expect(firstLine.childAt(1).prop('dur')).toEqual(0.5);
-      });
-
-      it('sets the fill to freeze', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-        const firstLine = svg().childAt(0);
-        expect(firstLine.childAt(1).prop('fill')).toEqual('freeze');
-      });
-    });
-
-    it('creates x2 and y2 animations for every line drawn', () => {
-      wrapper = mount(<Drawing drawCommands={[
-        { id: 123, x1: 10, y1: 10, x2: 20, y2: 20 },
-        { id: 234, x1: 10, y1: 10, x2: 20, y2: 20 },
-        { id: 345, x1: 10, y1: 10, x2: 20, y2: 20 },
-      ]}/>);
-      expect(wrapper.find('animate').length).toEqual(6);
-    });
-
-    describe('later draw commands', () => {
-      let timeSpy;
-      let root;
-
-      beforeEach(async () => {
-        timeSpy = jest.fn();
-        timeSpy.mockReturnValue(5);
-        SVGSVGElement.prototype.getCurrentTime = timeSpy;
-        root = document.createElement('div');
-        wrapper = mount(<Drawing drawCommands={ [  ] }/>);
-        wrapper.setProps({ drawCommands: [ lineA, lineB ] });
-        await new Promise(setTimeout);
-        wrapper = wrapper.update();
-      });
-
-      it('uses getCurrentTime function to find current document animation time on second render', () => {
-        expect(timeSpy).toHaveBeenCalled();
-      });
-
-      it('sets the first new draw command begin to the current time', () => {
-        expect(wrapper.find('line').at(0).childAt(0).prop('begin')).toEqual(5);
-      });
-
-      it('sets the next draw command to that time plus the duration', () => {
-        expect(wrapper.find('line').at(1).childAt(0).prop('begin')).toEqual(5.5);
-      });
-    });
+  it('does not adjust increments on existing lines when re-rendering', () => {
+    wrapper = mount(<Drawing drawCommands={ [ lineA, lineB ] }/>);
+    wrapper.setProps({ drawCommands: [ lineA, lineB, lineC ] });
+    const delays = wrapper.find('DrawingLine').map(line => line.prop('delay'));
+    expect(delays).toEqual([0, 500, 0]);
   });
 });
 
