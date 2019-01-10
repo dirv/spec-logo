3130d9c187304219ea38b9a2c02d5928ce148172
Daniel Irvine <daniel@danielirvine.com>
2019-01-10 15:18:02 +0000

    Add in turtle with animation

--- src/Display.js
+++ src/Display.js
@@ -2,34 +2,108 @@ import React from 'react';
 import { useMappedState } from 'redux-react-hook';
 const { useCallback, useRef, useState } = React;
 
+export const Turtle = ({ animationCommands: { movement, rotate }, turtle }) => {
+  const buildPoints = (x, y) => `${x - 5},${y + 5}, ${x},${y - 7}, ${x + 5},${y + 5}`;
+
+  const buildRotation = (angle, x, y) => `${angle + 90}, ${x}, ${y}`;
+
+  return <polygon
+    points={buildPoints(turtle.x,turtle.y)}
+    fill="green"
+    strokeWidth="2"
+    stroke="black"
+    transform={`rotate(${buildRotation(turtle.angle, turtle.x, turtle.y)})`}>
+    {movement.map(({ id, x1, y1, x2, y2, begin, duration, angle }) => {
+        return <React.Fragment key={id}>
+        <animate attributeName="points"
+          begin={begin}
+          dur={duration}
+          fill="freeze"
+          to={buildPoints(x2, y2)} />
+        <animateTransform attributeName="transform"
+          type="rotate"
+          begin={begin}
+          dur={duration}
+          fill="freeze"
+          to={buildRotation(angle, x2, y2)} />
+        </React.Fragment>
+    })}
+    {rotate.map(({ oldAngle, newAngle, x, y, duration, begin }, i) => {
+        return <animateTransform
+          key={i}
+          attributeName="transform"
+          type="rotate"
+          begin={begin}
+          dur={duration}
+          fill="freeze"
+          to={buildRotation(newAngle, x, y)} />
+    })}
+    </polygon>;
+};
+
 const distance = ({ x1, y1, x2, y2 }) => Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
-export const AnimatedCommands = ({ drawCommands, startTime }) => {
-  const speed = 0.005;
+function drawLineAnimationTimes(command, { movement, currentTime, turtle }) {
+  const movementSpeed = 0.005;
+  const duration = distance(command) * movementSpeed;
+  return {
+    movement: [
+      ...movement,
+      {
+        begin: currentTime,
+        duration,
+        angle: turtle.angle,
+        x1: command.x1,
+        y1: command.y1,
+        x2: command.x2,
+        y2: command.y2,
+        id: command.id
+      }
+    ],
+    currentTime: currentTime + duration,
+    turtle: { ...turtle, x: command.x2, y: command.y2 }
+  };
+}
+
+function rotationAnimationTimes(command, { rotate, currentTime, turtle }) {
+  const rotateSpeed = 2 / 360;
+  const duration = Math.abs(command.angle - turtle.angle) * rotateSpeed;
+  return {
+    rotate: [
+      ...rotate,
+      { begin: currentTime,
+        duration,
+        x: turtle.x,
+        y: turtle.y,
+        oldAngle: turtle.angle,
+        newAngle: command.angle
+      }
+    ],
+    currentTime: currentTime + duration,
+    turtle: { ...turtle, angle: command.angle }
+  };
+}
+export const AnimatedCommands = ({ drawCommands, startTime, turtle }) => {
   const drawLineCommands = drawCommands.filter( command => command.drawCommand === 'drawLine');
 
-  const animationTimes = drawLineCommands.reduce((times, command) => {
-    const duration = distance(command) * speed;
-    return {
-      lines: {
-        ...times.lines,
-        [command.id]: {
-          begin: times.currentTime,
-          duration
-        }
-      },
-      currentTime: times.currentTime + duration
-    };
-  }, { lines: {}, currentTime: startTime }).lines;
+  const animationCommands = drawCommands.reduce((times, command) => {
+    if (command.drawCommand === 'drawLine') {
+      return { ...times, ...drawLineAnimationTimes(command, times) };
+    } else if (command.drawCommand === 'rotate') {
+      return { ...times, ...rotationAnimationTimes(command, times) };
+    } else {
+      return times;
+    }
+  }, { movement: [], rotate: [], currentTime: startTime, turtle });
 
   return (
     <React.Fragment>
-      {drawLineCommands.map(({ id, x1, y1, x2, y2 }) => {
-        const times = animationTimes[id];
+      {animationCommands.movement.map(({ id, x1, y1, x2, y2, begin, duration }) => {
         return <line key={id} x1={x1} y1={y1} x2={x1} y2={y1} strokeWidth="2" stroke="black">
-          <animate attributeName="x2" begin={times.begin} dur={times.duration} to={x2} fill="freeze" />
-          <animate attributeName="y2" begin={times.begin} dur={times.duration} to={y2} fill="freeze" />
+          <animate attributeName="x2" begin={begin} dur={duration} to={x2} fill="freeze" />
+          <animate attributeName="y2" begin={begin} dur={duration} to={y2} fill="freeze" />
          </line>})}
+      <Turtle animationCommands={animationCommands} turtle={turtle} />
     </React.Fragment>
   );
 };
@@ -41,6 +115,17 @@ export const StaticCommands = ({ drawCommands }) => {
       return <line key={id} x1={x1} y1={y1} x2={x2} y2={y2} strokeWidth="2" stroke="black" />})}
     </React.Fragment>;
 };
+
+function calculateLastTurtlePosition(previousCommands) {
+  const reversedCommands = previousCommands.reverse();
+  const lastDrawLineCommand = reversedCommands.find(command => command.drawCommand === 'drawLine');
+  const lastRotation = reversedCommands.find(command => command.drawCommand === 'rotate');
+  return {
+    x: lastDrawLineCommand ? lastDrawLineCommand.x2 : 0,
+    y: lastDrawLineCommand ? lastDrawLineCommand.y2 : 0,
+    angle: lastRotation ? lastRotation.angle : 0
+  };
+}
 
 export const Drawing = ({ drawCommands }) => {
   const [ previousDrawCommands, setPreviousDrawCommands ] = useState([]);
@@ -60,13 +145,16 @@ export const Drawing = ({ drawCommands }) => {
     currentTime = 0;
   }
 
+  const previousCommands = drawCommands.slice(0, firstCommandToAnimate);
+
   return (
     <div id="viewport">
       <svg viewBox="-300 -300 600 600" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg" ref={svgRef}>
-        <StaticCommands drawCommands={drawCommands.slice(0, firstCommandToAnimate)} />
+        <StaticCommands drawCommands={previousCommands} />
         <AnimatedCommands
           drawCommands={drawCommands.slice(firstCommandToAnimate)}
-          startTime={currentTime} />
+          startTime={currentTime}
+          turtle={calculateLastTurtlePosition(previousCommands)} />
       </svg>
     </div>
   );
--- test/Display.test.js
+++ test/Display.test.js
@@ -8,7 +8,9 @@ import { StaticCommands, AnimatedCommands, Drawing, ReduxConnectedDisplay } from
 
 let lineA = { drawCommand: 'drawLine', id: 123, x1: 10, y1: 10, x2: 20, y2: 10 };
 let lineB = { drawCommand: 'drawLine', id: 234, x1: 20, y1: 10, x2: 20, y2: 20 };
-let lineC = { drawCommand: 'drawLine', id: 235, x1: 20, y1: 20, x2: 30, y2: 30 };
+let lineC = { drawCommand: 'drawLine', id: 345, x1: 20, y1: 20, x2: 30, y2: 30 };
+let rotate90 = { drawCommand: 'rotate', id: 456, angle: 90 };
+let rotate180 = { drawCommand: 'rotate', id: 456, angle: 180 };
 
 describe('Drawing', () => {
   let store;
@@ -45,34 +47,6 @@ describe('Drawing', () => {
     expect(svg().find('svg line').prop('y1')).toEqual(10);
   });
 
-  describe('AnimatedCommands', () => {
-    it('initially sets end of line to beginning of line, ready for animation', () => {
-      wrapper = mountSvg(<AnimatedCommands drawCommands={ [ lineA ] } startTime={0} />);
-      expect(svg().find('line').prop('x2')).toEqual(10);
-      expect(svg().find('line').prop('y2')).toEqual(10);
-    });
-
-    it('draws every drawLine command', () => {
-      wrapper = mountSvg(<AnimatedCommands drawCommands={ [ lineA, lineB, lineC ] } startTime={0} />);
-      expect(svg().find('line').length).toEqual(3);
-    });
-
-    it('does not draw any commands for non-drawLine commands', () => {
-      wrapper = mountSvg(<AnimatedCommands drawCommands={ [ { drawCommand: 'unknown' } ] } startTime={0} />);
-      expect(svg().find('line').length).toEqual(0);
-    });
-
-    it('sets a stroke width of 2 on each line', () => {
-      wrapper = mountSvg(<AnimatedCommands drawCommands={ [ lineA ] } startTime={0} />);
-      expect(svg().find('line').prop('strokeWidth')).toEqual('2');
-    });
-
-    it('sets a stroke color of black on each line', () => {
-      wrapper = mountSvg(<AnimatedCommands drawCommands={ [ lineA ] } startTime={0} />);
-      expect(svg().find('line').prop('stroke')).toEqual('black');
-    });
-  });
-
   describe('StaticCommands', () => {
     it('renders lines with correct co-ordinate values', () => {
       wrapper = mountSvg(<StaticCommands drawCommands={ [lineA ] } />);
@@ -88,51 +62,77 @@ describe('Drawing', () => {
     });
 
     it('draws every drawLine command', () => {
-      wrapper = mountSvg(<Drawing drawCommands={ [ lineA, lineB, lineC ] }/>);
+      wrapper = mountSvg(<StaticCommands drawCommands={ [ lineA, lineB, lineC ] }/>);
       expect(svg().find('line').length).toEqual(3);
     });
 
     it('does not draw any commands for non-drawLine commands', () => {
-      wrapper = mountSvg(<AnimatedCommands drawCommands={ [ { drawCommand: 'unknown' } ] }/>);
+      wrapper = mountSvg(<StaticCommands drawCommands={ [ { drawCommand: 'unknown' } ] }/>);
       expect(svg().find('line').length).toEqual(0);
     });
 
     it('sets a stroke width of 2 on each line', () => {
-      wrapper = mountSvg(<Drawing drawCommands={ [ lineA ] }/>);
+      wrapper = mountSvg(<StaticCommands drawCommands={ [ lineA ] }/>);
       expect(svg().find('line').prop('strokeWidth')).toEqual('2');
     });
 
     it('sets a stroke color of black on each line', () => {
-      wrapper = mountSvg(<Drawing drawCommands={ [ lineA ] }/>);
+      wrapper = mountSvg(<StaticCommands drawCommands={ [ lineA ] }/>);
       expect(svg().find('line').prop('stroke')).toEqual('black');
     });
   });
 
   describe('animating', () => {
-    describe('x2 coordinate', () => {
-      it('renders animate element', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
-        const firstLine = svg().find('svg line');
-        expect(firstLine.childAt(0).type()).toEqual('animate');
-        expect(firstLine.childAt(0).prop('attributeName')).toEqual('x2');
-        expect(firstLine.childAt(0).prop('to')).toEqual(20);
+    const initialTurtle = { x: 0, y: 0, angle: 0 };
+
+    function mountAnimatedCommands(props) {
+      return mountSvg(<AnimatedCommands startTime={0} turtle={initialTurtle} {...props} />);
+    }
+
+    describe('line svg elements', () => {
+      it('initially sets end of line to beginning of line, ready for animation', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
+        expect(svg().find('line').prop('x2')).toEqual(10);
+        expect(svg().find('line').prop('y2')).toEqual(10);
       });
 
+      it('draws every drawLine command', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA, lineB, lineC ] });
+        expect(svg().find('line').length).toEqual(3);
+      });
+
+      it('does not draw any commands for non-drawLine commands', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ { drawCommand: 'unknown' } ] });
+        expect(svg().find('line').length).toEqual(0);
+      });
+
+      it('sets a stroke width of 2 on each line', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
+        expect(svg().find('line').prop('strokeWidth')).toEqual('2');
+      });
+
+      it('sets a stroke color of black on each line', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
+        expect(svg().find('line').prop('stroke')).toEqual('black');
+      });
+    });
+
+    describe('x2 coordinate', () => {
       it('sets the begin time to 0 when component first mounts', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
         const firstLine = svg().find('svg line');
         expect(firstLine.childAt(0).prop('begin')).toEqual(0);
       });
 
       describe('duration', () => {
         it('has a duration of 0.05s for a horizontal line of length 10', () => {
-          wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
           const firstLine = svg().find('svg line');
           expect(firstLine.childAt(0).prop('dur')).toEqual(0.05);
         });
 
         it('has a duration based on the distance for any line', () => {
-          wrapper = mount(<Drawing drawCommands={ [ lineC ] }/>);
+          wrapper = mountAnimatedCommands({ drawCommands: [ lineC ] });
           const distance = Math.sqrt(10 * 10 * 2);
           const firstLine = svg().find('svg line');
           expect(firstLine.childAt(0).prop('dur')).toEqual(distance * 0.005);
@@ -140,7 +140,7 @@ describe('Drawing', () => {
       });
 
       it('sets the fill to freeze', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
         const firstLine = svg().find('svg line');
         expect(firstLine.childAt(0).prop('fill')).toEqual('freeze');
       });
@@ -148,7 +148,7 @@ describe('Drawing', () => {
 
     describe('y2 coordinate', () => {
       it('renders animate element', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
         const firstLine = svg().find('svg line').at(0);
         expect(firstLine.childAt(1).type()).toEqual('animate');
         expect(firstLine.childAt(1).prop('attributeName')).toEqual('y2');
@@ -156,59 +156,147 @@ describe('Drawing', () => {
       });
 
       it('sets the begin time to 0 when component first mounts', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
         const firstLine = svg().find('svg line');
         expect(firstLine.childAt(1).prop('begin')).toEqual(0);
       });
 
       it('has a duration of 0.05s', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
         const firstLine = svg().find('svg line');
         expect(firstLine.childAt(1).prop('dur')).toEqual(0.05);
       });
 
       it('sets the fill to freeze', () => {
-        wrapper = mount(<Drawing drawCommands={ [ lineA ] }/>);
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
         const firstLine = svg().find('svg line');
         expect(firstLine.childAt(1).prop('fill')).toEqual('freeze');
       });
     });
 
-    it('creates x2 and y2 animations for every line drawn', () => {
-      wrapper = mount(<Drawing drawCommands={[
-        { drawCommand: 'drawLine', id: 123, x1: 10, y1: 10, x2: 20, y2: 20 },
-        { drawCommand: 'drawLine', id: 234, x1: 10, y1: 10, x2: 20, y2: 20 },
-        { drawCommand: 'drawLine', id: 345, x1: 10, y1: 10, x2: 20, y2: 20 },
-      ]}/>);
-      expect(wrapper.find('animate').length).toEqual(6);
+    describe('Turtle', () => {
+
+      function turtle() {
+        return svg().find('polygon');
+      }
+
+      it('renders a polygon of an isosceles triangle shape at the current turtle position', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [], turtle: { x: 10, y: 10, angle: 0 } });
+        expect(turtle().exists()).toBeTruthy();
+        expect(turtle().prop('points')).toEqual('5,15, 10,3, 15,15');
+      });
+
+      it('renders a polygon with a stroke and fill', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [], turtle: { x: 10, y: 10, angle: 0 } });
+        expect(turtle().prop('fill')).toEqual('green');
+        expect(turtle().prop('strokeWidth')).toEqual('2');
+        expect(turtle().prop('stroke')).toEqual('black');
+      });
+
+      it('renders a polygon with an initial rotation to the initial turtle angle', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [], turtle: { x: 10, y: 20, angle: 90 } });
+        expect(turtle().prop('transform')).toEqual('rotate(180, 10, 20)');
+      });
+
+      it('renders an animateTransform for a rotate command', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ rotate90 ] });
+        expect(turtle().find('animateTransform').exists()).toBeTruthy();
+        expect(turtle().find('animateTransform').prop('attributeName')).toEqual('transform');
+        expect(turtle().find('animateTransform').prop('type')).toEqual('rotate');
+        expect(turtle().find('animateTransform').prop('fill')).toEqual('freeze');
+      });
+
+      it('sets the duration to 1s for 180 degree turn', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ rotate180 ] });
+        expect(turtle().find('animateTransform').prop('dur')).toEqual(1);
+      });
+
+      it('sets the duration to 0.5s for 90 degree turn', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ rotate90 ] });
+        expect(turtle().find('animateTransform').prop('dur')).toEqual(0.5);
+      });
+
+      it('renders an animate element for points for a drawLine command', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
+        expect(turtle().find('animate').exists()).toBeTruthy();
+        expect(turtle().find('animate').prop('attributeName')).toEqual('points');
+        expect(turtle().find('animate').prop('fill')).toEqual('freeze');
+      });
+
+      it('sets the movement to the x2, y2 coordinate of the line', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
+        expect(turtle().find('animate').prop('to')).toEqual('15,15, 20,3, 25,15');
+      });
+
+      it('renders an animateTransform element for the rotation of a drawLine command', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
+        expect(turtle().find('animateTransform').exists()).toBeTruthy();
+        expect(turtle().find('animateTransform').prop('attributeName')).toEqual('transform');
+        expect(turtle().find('animateTransform').prop('type')).toEqual('rotate');
+        expect(turtle().find('animateTransform').prop('fill')).toEqual('freeze');
+      });
+
+      it('sets the rotation to the angle and end position of the line', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineC ], turtle: { x: 20, y: 20, angle: 45 } });
+        expect(turtle().find('animateTransform').prop('to')).toEqual('135, 30, 30');
+      });
+
+      it('sets the duration to the same duration that the drawLine transform occurs at', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA ] });
+        const expectedDur = wrapper.find('line > animate').at(0).prop('dur');
+        expect(turtle().find('animate').prop('dur')).toEqual(expectedDur);
+        expect(turtle().find('animateTransform').prop('dur')).toEqual(expectedDur);
+      });
+
+      it('renders an animate element and an animateTransform element for each line drawn', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA, lineB, lineC ] });
+        expect(turtle().find('animate').length).toEqual(3);
+        expect(turtle().find('animateTransform').length).toEqual(3);
+      });
+
+      it('renders an animateTransform element for each rotation', () => {
+        wrapper = mountAnimatedCommands({ drawCommands: [ lineA, lineB, lineC ] });
+        expect(turtle().find('animateTransform').length).toEqual(3);
+      });
     });
 
-    describe('later draw commands', () => {
-      let timeSpy;
-      let root;
+    it('begins the animation at the start time', () => {
+      wrapper = mountAnimatedCommands({ drawCommands: [ lineA ], startTime: 5 });
+      expect(wrapper.find('line').at(0).childAt(0).prop('begin')).toEqual(5);
+    });
 
-      beforeEach(async () => {
-        timeSpy = jest.fn();
-        timeSpy.mockReturnValue(5);
-        SVGSVGElement.prototype.getCurrentTime = timeSpy;
-        root = document.createElement('div');
-        wrapper = mount(<Drawing drawCommands={ [  ] }/>);
-        wrapper.setProps({ drawCommands: [ lineA, lineB ] });
-        await new Promise(setTimeout);
-        wrapper = wrapper.update();
-      });
+    it('sets animations to run consecutively', () => {
+      wrapper = mountAnimatedCommands({ drawCommands: [ lineA, rotate90, lineB ], startTime: 5 });
+      const beginDrawTimes = wrapper.find('line > animate[attributeName="x2"]').map(a => a.prop('begin'));
+      const beginMoveTurtleTimes = wrapper.find('polygon > animate').map(a => a.prop('begin'));
+      const beginRotateTurtleTimes = wrapper.find('polygon > animateTransform').map(a => a.prop('begin'));
+      expect(beginDrawTimes).toEqual([5, 5.55]);
+      expect(beginMoveTurtleTimes).toEqual([5, 5.55]);
+      expect(beginRotateTurtleTimes).toEqual([5, 5.55, 5.05]);
+    });
+  });
 
-      it('uses getCurrentTime function to find current document animation time on second render', () => {
-        expect(timeSpy).toHaveBeenCalled();
-      });
+  describe('second render', () => {
+    let timeSpy;
+    let root;
 
-      it('sets the first new draw command begin to the current time', () => {
-        expect(wrapper.find('line').at(0).childAt(0).prop('begin')).toEqual(5);
-      });
+    beforeEach(async () => {
+      timeSpy = jest.fn();
+      timeSpy.mockReturnValue(5);
+      SVGSVGElement.prototype.getCurrentTime = timeSpy;
+      root = document.createElement('div');
+      wrapper = mount(<Drawing drawCommands={ [  ] }/>);
+      wrapper.setProps({ drawCommands: [ lineA, lineB ] });
+      await new Promise(setTimeout);
+      wrapper = wrapper.update();
+    });
 
-      it('sets the next draw command to that time plus the duration', () => {
-        expect(wrapper.find('line').at(1).childAt(0).prop('begin')).toEqual(5.05);
-      });
+    it('uses getCurrentTime function to find current document animation time on second render', () => {
+      expect(timeSpy).toHaveBeenCalled();
+    });
+
+    it('passes the time to AnimatedCommands as startTime', () => {
+      expect(wrapper.find('AnimatedCommands').prop('startTime')).toEqual(5);
     });
   });
 });
